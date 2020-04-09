330e84249a662688250cca2f59fa5ada9122f083
olthar <48121104+olthar@users.noreply.github.com>
2020-04-09 12:20:50 +0100

    Work on unit 1 project

--- treehouseProjects/Project1_ARandomQuoteGenerator/js/RandomQuote.js
+++ treehouseProjects/Project1_ARandomQuoteGenerator/js/RandomQuote.js
@@ -2,8 +2,14 @@
 
 // Quotes in an array
 const quotes = [
-    {quote: "test", source:"test", citation: "", year: 1982},
-];
+    {quote: "They always say start at the bottom if you want to learn something. But suppose you want to learn to swim?", source:"Tommy Cooper", citation: "" },
+    {quote: "Do or do not, there is no try", source:"Yoda", citation: "The Empire Strikes Back", year: "A long time ago"},
+    {quote: "I always knew I’d end up working for a Mickey Mouse outfit", source:"Disney employee", citation: "", year: ""},
+    {quote: "To be happy at home is the end of all human endeavour", source:"Samuel Johnson", citation: "", year: 1750},
+    {quote: "It Doesn’t Matter Who You Love or How You Love, But That You Love", source:"Rod McKuen", citation: "New York Times", year: 1971},
+    {quote: "Forgiveness is giving up all hope of a better past.", source:"Rev. Don Felt", citation: "The Los Angeles Times", year: 1991},
+    {quote: "In order to be irreplaceable one must always be different.", source:"Coco Chanel", citation: "Her Life, Her Secrets by Marcel Haedrich", year: 1971}
+]
 let quote;
 //Create a funcation that creates HTML
 function formQuote(){
--- treehouseProjects/Project1_ARandomQuoteGenerator/js/script.js
+++ treehouseProjects/Project1_ARandomQuoteGenerator/js/script.js
@@ -7,17 +7,44 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/*** 
- * `quotes` array 
-***/
 
+  const quotes = [
+    {quote: "They always say start at the bottom if you want to learn something. But suppose you want to learn to swim?", source:"Tommy Cooper"},
+    {quote: "Do or do not, there is no try", source:"Yoda", citation: "The Empire Strikes Back", year: "A long time ago"},
+    {quote: "I always knew I’d end up working for a Mickey Mouse outfit", source:"Disney employee"},
+    {quote: "To be happy at home is the end of all human endeavour", source:"Samuel Johnson", year: 1750},
+    {quote: "It Doesn’t Matter Who You Love or How You Love, But That You Love", source:"Rod McKuen", citation: "New York Times", year: 1971},
+    {quote: "Forgiveness is giving up all hope of a better past.", source:"Rev. Don Felt", citation: "The Los Angeles Times", year: 1991},
+    {quote: "In order to be irreplaceable one must always be different.", source:"Coco Chanel", citation: "Her Life, Her Secrets by Marcel Haedrich", year: 1971}
+]
+let message = printQuote(quotes);;
 
+function getRandomQuote(arr){
+   const randomNumber = Math.floor(Math.random() * (arr.length-1))+1;
+   const randomQuote = arr[randomNumber];
+   console.log(randomQuote.quote);
+   return randomQuote
+}
 
-/***
- * `getRandomQuote` function
-***/
+//Print quote function returns html to @message
+function printQuote(i) {
+  const messageQuote = getRandomQuote(i);
+  let html = `<p class="quote"> ${messageQuote.quote} </p><p class="source"> ${messageQuote.source}`
+  for ( var prop in messageQuote ) {
+    console.log(prop);
+    if (prop === "citation"){
+      html += `<span class="citation"> ${messageQuote.citation}</span>`
+    } else if (prop === "year"){
+      html += `<span class="year"> ${messageQuote.year}</span>` 
+    } else {
+    continue
+    }
+  }
+  html += `</p>`
+  return html
+}
 
-
+document.getElementById('quote-box').innerHTML = message; 
 
 /***
  * `printQuote` function
